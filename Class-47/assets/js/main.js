c250aa84696f6c035678d9b724b33620288f8555
mahananda9889 <mahanandaray9889@gmail.com>
2017-06-08 02:25:25 +0600

    class47

---
+++ Class-47/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            nav: false,
+            autoplay: false,
+        });
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
