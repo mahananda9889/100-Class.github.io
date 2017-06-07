9304a30a00ce2cca3a6fb49e9cb7adb59ddcdbee
mahananda9889 <mahanandaray9889@gmail.com>
2017-06-08 00:47:22 +0600

    Class 46

---
+++ Class-46/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".featured-tailor-carousel").owlCarousel({
+            items: 1,
+            nav: false,
+            autoplay: false,
+            loop: true,
+            dots: true,
+        });
+
+    });
+ 
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
