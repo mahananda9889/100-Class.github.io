a3b209a17fdb6dac1406c84bedf93f46535bdad7
mahananda9889 <mahanandaray9889@gmail.com>
2017-06-08 15:33:24 +0600

    Class50

---
+++ Class-50/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".listing-carousel").owlCarousel({
+            items: 3,
+            loop: true,
+            nav: false,
+            dots: true,
+            autoplay: false,
+            margin: 30
+        });
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
